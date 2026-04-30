18f8d6dc7e07290fc4f80bd2fdddb8aa02d091b9
daniprograms-arch <daniprograms@gmail.com>
2026-04-29 22:02:46 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'bcv-converter-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
